3d6bee0cd8c498b720459584bbcfea6d15bac20a
Penguinatron <68402882+Penguinatron@users.noreply.github.com>
2020-08-20 23:02:37 +0100

    Duh update
    Fixed some very simple, late night bugs.

--- !View.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mv ~/Desktop/"Software Projects"/Website/"Working Folder"/index.txt ~/Desktop/"Software Projects"/Website/"Working Folder"/index.html
-
-open ~/Desktop/"Software Projects"/Website/"Working Folder"/index.html
-
-Read -p "Close window, then press enter to continue editing."
-
-mv ~/Desktop/"Software Projects"/Website/"Working Folder"/index.html ~/Desktop/"Software Projects"/Website/"Working Folder"/index.txt
