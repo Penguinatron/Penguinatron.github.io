07631ff58de7036cf16e900f6df96a01de5a3b7a
Penguinatron <68402882+Penguinatron@users.noreply.github.com>
2020-08-20 22:55:40 +0100

    My first commit
    Trying to put a website on the internet, at least briefly, as a test. This is exciting!

---
+++ !View.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mv ~/Desktop/"Software Projects"/Website/"Working Folder"/index.txt ~/Desktop/"Software Projects"/Website/"Working Folder"/index.html
+
+open ~/Desktop/"Software Projects"/Website/"Working Folder"/index.html
+
+Read -p "Close window, then press enter to continue editing."
+
+mv ~/Desktop/"Software Projects"/Website/"Working Folder"/index.html ~/Desktop/"Software Projects"/Website/"Working Folder"/index.txt
